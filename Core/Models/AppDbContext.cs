0bc8f96ff1439f0173fea736bdb670d56931bc8a
Ha Phi Hung <haphihung55@gmail.com>
2023-10-26 14:41:52 +0700

    Implement models

---
+++ Core/Models/AppDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Core.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public AppDbContext(DbContextOptions builder)
+            : base(builder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Category>().HasData(
+                    new Category { Id = 1, Name = "Beverages" },
+                    new Category { Id = 2, Name = "Condiments" },
+                    new Category { Id = 3, Name = "Confections" },
+                    new Category { Id = 4, Name = "Dairy products" },
+                    new Category { Id = 5, Name = "Grains/Cereals" },
+                    new Category { Id = 6, Name = "Meat/Poultry" },
+                    new Category { Id = 7, Name = "Produce" },
+                    new Category { Id = 8, Name = "Seafood" }
+                );
+
+            builder.Entity<Product>().HasData(
+                    new Product
+                    {
+                        Id = 1,
+                        Name = "CocaCola",
+                        CategoryId = 1,
+                        UnitPrice = 10000,
+                        UnitsInStock = 5
+                    },
+                    new Product
+                    {
+                        Id = 2,
+                        Name = "Milo",
+                        CategoryId = 4,
+                        UnitPrice = 8000,
+                        UnitsInStock = 10
+                    }
+                );
+        }
+    }
+}
---
+++ Core/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public required string Name { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; } = null!;
+
+        [Required]
+        public int UnitsInStock { get; set; }
+
+        [Required]
+        public decimal UnitPrice { get; set; }
+    }
+}
